8461a6bf0b382f86ad0ce21593ba24f8f45b0f4d
Pablo M. Iglesias <alkhymia@gmail.com>
2023-09-15 16:59:42 -0300

    init commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleForm() {
+  var container = document.querySelector(".container");
+  var section = document.querySelector("section");
+  container.classList.toggle("active");
+  section.classList.toggle("active");
+}
